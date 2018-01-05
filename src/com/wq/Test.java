d3209ce87ef5cf21242e833302e2aa2c717e02e3
Wq <Administrator@6AZEWHB4M270HXG>
2018-01-05 16:45:48 +0800

    新建了Hello

---
+++ src/com/wq/Test.java
@@ -0,0 +1,15 @@
+package com.wq;
+
+/**  
+* @ClassName: Test  
+* @Description: TODO
+* @author Administrator  
+* @date 2018年1月5日  下午4:42:18
+*  
+*/
+public class Test {
+	
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+}
